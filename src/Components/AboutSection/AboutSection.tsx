f076c7e3b66f88ee03916565402d836f176a0507
Brett Kuhn <brettkuhn87@gmail.com>
2023-09-06 13:38:49 -0600

    Updates About Me

--- src/Components/AboutSection/AboutSection.tsx
+++ src/Components/AboutSection/AboutSection.tsx
@@ -22,22 +22,18 @@ const AboutSection = () => {
             Who I Am ...</h2>
             <div className='flex flex-col rounded-md border-2 border-stone-500 drop-shadow-2xl bg-stone-50 bg-opacity-60 backdrop-blur-sm backdrop-brightness-125'>
               <p className='font-font5 indent-8 text-justify w-full p-7 pb-1 text-sm md:text-xl text-stone-700 '>
-                Hello! I'm Brett Kuhn, a software engineer who specializes in front end development and a recent graduate of 
-                Turing School of Software and Design. 
+              After managing projects in the oil & gas industry for nearly a decade, my desire for solving problems and finding new challenges led me to software development and in 2022, I enrolled in the Turing School of Software & Design. 
               </p>
-              <p className='font-font5 indent-8 text-justify w-full p-7 pt-3 text-sm md:text-xl text-stone-700 '>I'm passionate about building applications and solving complex 
-                problems, and I love new experiences and learning new things both inside and outside of the 
-                tech industry. Working as a part of a team is important to me, and I take pride in building a strong
-                culture. When I'm not coding, you can find me traveling, rock climbing, playing
+              <p className='font-font5 indent-8 text-justify w-full p-7 pt-3 text-sm md:text-xl text-stone-700 '>As a recent graduate and freelancer, I'm passionate about building apps with a strong emphasis on aesthetics and user experience, as well as building culture and connecting with others. I excel at working in a collaborative, deadline driven environment that requires a high level of coordination and attention to detail. When I'm not coding, you can find me traveling, rock climbing, playing
                 music or building guitars.</p>
             </div>
           <br />
           <h2 className='font-font7 text-center rounded-lg border-2 border-stone-800 drop-shadow-2xl  bg-stone-800 bg-opacity-90 backdrop-blur-sm p-3 pt-4 mb-2 text-xl md:text-4xl text-amber-500 bg-gradient-to-r from-neutral-800 to-stone-700'>
             What I'm Working On ...</h2>
           <ul className='font-font5 eading-6 rounded-md border-2 border-stone-500 drop-shadow-2xl text-left w-full bg-stone-50 bg-opacity-60 backdrop-blur-sm p-7 text-sm md:text-xl text-stone-700 backdrop-brightness-125'>
-            <li className='mx-6 list-disc list-outside'><b className='font-font4 text-stone-800'>Next project... </b> Producing a card game app in a four person front end team that will incorporate a back end database (GitHub repo coming soon).</li>
-            <li className='mx-6 list-disc list-outside'><b className='font-font4 text-stone-800'>Currently learning... </b> Next.js, Express.</li>
-            <li className='mx-6 list-disc list-outside'><b className='font-font4 text-stone-800'>What else? </b> Adding to this site! More features and more projects, and deploying previous student projects.</li>
+            <li className='mx-6 list-disc list-outside'><b className='font-font4 text-stone-800'>Next project... </b> A road trip app that incorporates map and event data.</li>
+            <li className='mx-6 list-disc list-outside'><b className='font-font4 text-stone-800'>Currently learning... </b> Next.js</li>
+            <li className='mx-6 list-disc list-outside'><b className='font-font4 text-stone-800'>What else? </b> Adding more features and more projects to this site, and deploying previous student projects.</li>
           </ul>
         </section>
       </div>
